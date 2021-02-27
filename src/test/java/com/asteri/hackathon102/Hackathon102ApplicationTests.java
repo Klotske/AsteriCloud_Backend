ec511dbe3015d393573b36222c56635d988efb99
NestoR-afk <yarocla2002@mail.ru>
2021-02-27 12:15:59 +0300

    finally

---
+++ src/test/java/com/asteri/hackathon102/Hackathon102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asteri.hackathon102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
